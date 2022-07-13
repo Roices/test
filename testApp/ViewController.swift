d5d87d4c04e348d09940dbd26e663910cf9af382
Roices <tuande83@gmail.com>
2022-07-13 22:05:47 +0700

    Day la Test

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let labelTest : UILabel = {
+       let label = UILabel(frame: CGRect(x: 50,
+                                         y: 100,
+                                         width: 100,
+                                         height: 100))
+        label.text = "Duonwg kute"
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(labelTest)
         // Do any additional setup after loading the view.
     }
 
