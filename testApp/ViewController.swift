be7f668160023515e984d43db2b2a3398a53cedd
Roices <tuande83@gmail.com>
2022-07-13 22:08:33 +0700

    Commit code

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -10,9 +10,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let buttonTest: UIButton() = {
+       let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        button.setTitleColor("Button", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(buttonTest)
         // Do any additional setup after loading the view.
     }
 
